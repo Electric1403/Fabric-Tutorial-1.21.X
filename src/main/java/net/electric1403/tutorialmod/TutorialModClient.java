dc8b564cf48483d7fd3baa75259bdd20c8882c5c
Electric1403 <cravedimatte@gmail.com>
2024-11-27 01:06:15 +0100

    Initial commit

---
+++ src/main/java/net/electric1403/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.electric1403.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
